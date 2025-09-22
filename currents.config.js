685055c121a4fd9649514494c7e357210f0c18c6
ththanyarat@centralpattana.co.th <@Kan123456789>
2025-09-22 17:44:07 +0700

    add docker

---
+++ currents.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    projectId: "e-tax-manage-cypress", // the projectId, can be any values for sorry-cypress users
+    recordKey: "yyy", // the record key, can be any value for sorry-cypress users
+    cloudServiceUrl: "http://localhost:1234",   // Sorry Cypress users - set the director service URL http://localhost:1234
+};
